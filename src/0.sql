b0051207954e6e01bdf4e5600c1fdde5bfd85e41
ytyaru <ytyaru@outlook.jp>
2019-09-05 12:50:12 +0900

    初回コミット

---
+++ src/0.sql
@@ -0,0 +1,12 @@
+select NULL=1;
+select 1=NULL;
+select NULL=NULL;
+
+select NULL is 1;
+select 1 is NULL;
+select NULL is NULL;
+
+select NULL is not 1;
+select 1 is not NULL;
+select NULL is not NULL;
+
